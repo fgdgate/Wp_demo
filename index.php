4d06d4275319367e6abe7fe8f5992c0f109441f7
fgdgate <fgd.gert.d111@outlook.jp>
2021-10-23 21:42:17 +0900

    index変更

--- index.php
+++ index.php
@@ -1,7 +1,4 @@
 <!-- 記事を取得し、その記事のタイトルや画像を表示させている内容（26行まで） -->
-<?php
-include_once("index.html");
-?>
 <?php get_header(); ?>
 	<div class="index__img">
 		<img src="<?php bloginfo('template_url'); ?>/img\Campus.jpg" alt="">

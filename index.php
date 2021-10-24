0418ea8cc37243782c9d3556de1fd25ff493ea60
fgdgate <fgd.gert.d111@outlook.jp>
2021-10-24 16:15:52 +0900

    コメントアウト

--- index.php
+++ index.php
@@ -4,28 +4,26 @@
 		<div class="index-text">News List</div>
 	</div>
 	
-<!-- 記事ループ -->
 <?php if (have_posts()) :
 	while (have_posts()) :
 		the_post(); ?>
 
-		<!-- 記事のリンク -->
 		<a href="<?php the_permalink(); ?>">
-			<!-- アイキャッチ画像 -->
+			
 			<?php the_post_thumbnail('full'); ?>
-			<!-- タイトル -->
+			
 			<div class="index__title">
 				<p>
 					<?php the_title(); ?>
 				</p>
 			</div>
-			<!-- 投稿日 -->
+			
 			<div class="index__time">
 				<time datetime="<?php echo get_the_date('Y-m-d'); ?>">
 					<?php echo get_the_date(); ?>
 				</time>
 			</div>
-			<!-- 抜粋 -->
+			
 			<div class="index__exc">
 				<p>
 					<?php the_excerpt(); ?>
